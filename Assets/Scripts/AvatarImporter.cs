4e455dee75a27550282d3dae62862e8e1a5f5902
Ashray Pai <ashray@immersive-insiders.com>
2022-02-22 19:57:22 +0530

    Imported RPM SDK and setup scene for WebView

---
+++ Assets/Scripts/AvatarImporter.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.Events;
+using Wolf3D.ReadyPlayerMe.AvatarSDK;
+
+public class AvatarImporter : MonoBehaviour
+{
+    [SerializeField] private WebView webView;
+
+    private GameObject importedAvatar;
+
+    public GameObject ImportedAvatar
+    {
+        get
+        {
+            return importedAvatar;
+        }
+        set
+        {
+            importedAvatar = value;
+        }
+    }
+
+    private void Start()
+    {
+        webView.CreateWebView();
+        webView.OnAvatarCreated = ImportAvatar;
+    }
+
+    private void ImportAvatar(string url)
+    {
+        AvatarLoader avatarLoader = new AvatarLoader();
+        avatarLoader.LoadAvatar(url, null,StoreAvatar);
+    }
+
+    private void StoreAvatar(GameObject avatar, AvatarMetaData meta)
+    {
+        importedAvatar = avatar;
+        importedAvatar.transform.localScale = Vector3.one * 0.2f;
+        importedAvatar.SetActive(false);
+    }
+
+}
